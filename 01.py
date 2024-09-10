2c1e2be39e2e44a20dc8cfeda17e6b58592bf34c
liguanglong <ilong88@outlook.com>
2024-09-10 19:11:12 +0800

    获取信息

--- 01.py
+++ 01.py
@@ -15,22 +15,54 @@ class MyWidget(QWidget):
         self.init_ui()
     
     # 获取的用户名
+    def get_username(self):
+        return self.ui.lineEdit_1.text()
    
     # 获取的密码
+    def get_password(self):
+        return self.ui.lineEdit_2.text()
     
     # 获取的性别
-    
+    def get_gender(self):
+        if self.ui.radioButton_1.isChecked():
+            return "男"
+        elif self.ui.radioButton_2.isChecked():
+            return "女"
+        
     # 获取的爱好
+    def get_hobby(self):
+        hobbies = []
+        if self.ui.checkBox_1.isChecked():
+            hobbies.append("抽烟")
+        if self.ui.checkBox_2.isChecked():
+            hobbies.append("喝酒")
+        if self.ui.checkBox_3.isChecked():
+            hobbies.append("烫头")
+        
+        # 格式化元组
+        return  ",".join(hobbies)
+
+
+
+
+
     
     # 获取的个性签名
+    def get_signature(self):
+        return self.ui.lineEdit_3.text()
     
     # 择偶要求
-    
+    def get_sex_requirement(self):
+        return self.ui.plainTextEdit_1.toPlainText()
     
         
     def zuce(self):
-        print("按钮点击!")
-        print(self.ui.lineEdit_1.text())
+        print("用户名:", self.ui.lineEdit_1.text())
+        print("密码:", self.ui.lineEdit_2.text())
+        print("性别:", self.get_gender())
+        print("爱好:", self.get_hobby())
+        print("个性签名:", self.get_signature())
+        print("择偶要求:", self.get_sex_requirement())
         
     def init_ui(self):
         self.ui.pushButton.clicked.connect(self.zuce)
@@ -40,5 +72,7 @@ if __name__ == '__main__':
     widget = MyWidget()
     widget.show()
     sys.exit(app.exec_())
+
+
 
 
